c2b83581894eafa6242fec75bffdf3eb1bbfeb60
Melby <mariammelby@gmail.com>
2024-12-12 16:20:22 +0530

    added selectionhalf

--- frontend/src/SelectionPage.jsx
+++ frontend/src/SelectionPage.jsx
@@ -1,9 +1,29 @@
-export default function SelectionPage(){
+const SelectionPage = ()=>{
     return (
         <>
-            <div>
-                <h2>Selection Page</h2>
+        <div className="heading">
+            <h1 className="title">Selection Page</h1>
+            <h3 className="tagline">Choose Your Path: Generate, Rate, or Train – Master Every Stage of Your Presentation Journey.</h3>
+        </div>
+        <div className="page-container">
+          <div className="card-container">
+            <div className="card">
+              <h3>Presentation Generation</h3>
+              <p>This is the description for Card 1.</p>
             </div>
+            <div className="card">
+              <h3>Presentation Rating</h3>
+              <p>This is the description for Card 2.</p>
+            </div>
+            <div className="card">
+              <h3>Presentation Training</h3>
+              <p>This is the description for Card 3.</p>
+            </div>
+          </div>
+        </div>
         </>
-    )
-}
+      );
+    };
+
+
+export default SelectionPage;
