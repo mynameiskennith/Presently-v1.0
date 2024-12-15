47351b38cd785a0244af19a90ed2aa0a9b63055b
Akshay-C-A <akshayanil3103@gmail.com>
2024-12-16 01:41:10 +0530

    presently in navbar redirected to landing page

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return ( 
         <nav className="navbar">
-            <h1>Presently.</h1>
+            <Link to="/">
+                <h1>Presently.</h1>
+            </Link>
             <div className="links">
                 {/* <Link to="/">Home</Link>
                 <Link to="/create">New Blog</Link> */
