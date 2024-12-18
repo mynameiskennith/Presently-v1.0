828004504fedad3c6c982edf9aaaf54c4ce49eaa
Melby <mariammelby@gmail.com>
2024-12-18 22:40:49 +0530

    added tailwind in generation and downloading ppt section

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,22 +1,25 @@
 import { Link } from 'react-router-dom';
 
-
 const Navbar = () => {
     return ( 
-        <nav className="navbar">
+        <nav className="navbar bg-black p-4 flex items-center max-w-full mx-auto border-b border-white font-Quicksand">
             <Link to="/">
-                <h1>Presently.</h1>
+                <h1 className="text-white text-3xl font-extrabold">Presently.</h1>
             </Link>
-            <div className="links">
-                {/* <Link to="/">Home</Link>
-                <Link to="/create">New Blog</Link> */
-                // <><Link to="/signup">Sign Up</Link><Link to="/login">Login</Link></>
-                <><Link to="/generate">Generate PPT</Link><Link to="/rating">Rate PPT</Link></>
-
-                }
+            <div className="links ml-auto text-xl font-bold text-white">
+                <Link 
+                    to="/generate" 
+                    className="ml-4 text-white hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 py-2 px-4">
+                    Generate PPT
+                </Link>
+                <Link 
+                    to="/rating" 
+                    className="ml-4 text-white hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 py-2 px-4">
+                    Rate PPT
+                </Link>
             </div>
         </nav>
-     );
+    );
 }
  
 export default Navbar;
